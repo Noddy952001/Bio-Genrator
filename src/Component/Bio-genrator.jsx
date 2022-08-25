686782b958610fdf2c4abee5de3c67bcc3e2d232
vikas <vikasnwrdr2001@gmail.com>
2022-08-25 18:56:49 +0530

    set random name

--- src/Component/Bio-genrator.jsx
+++ src/Component/Bio-genrator.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import { useState } from "react";
 import "./Bio-genrator.css"
 
+
+
+
 export const Bio_Genrator = () => {
 
     const [image , setimage] = useState("https://tse2.mm.bing.net/th?id=OIP.vvjbgjel2AatUUhUOdHISgHaHa&pid=Api&P=0")
+    const [name , setname] = useState()
+    const [gender , setgender] = useState()
 
 
     const UploadImage=(e)=>{
@@ -12,6 +17,11 @@ export const Bio_Genrator = () => {
         setimage(URL.createObjectURL(file));
     }
     console.log(image)
+
+    const RandomName=()=>{
+        let Randomname=["vikas","raj","sameer","mohit" , "savita" ]
+        setname(Randomname[Math.floor(Math.random()*5)])
+    }
 
 
 
@@ -27,33 +37,59 @@ export const Bio_Genrator = () => {
                   <p> Option </p> 
 
                     <div className="Profile_box">
-                        <label>Profile photo</label>
-                        <input type="file"
-                            onChange={UploadImage}
-                        ></input>
-
-                                
+                        <span className="Profile">Profile photo</span>
+                        <span className="Profilefile">
+                            <input type="file"
+                                onChange={UploadImage}
+                            ></input>
+                        </span>
                     </div>
 
+                    <div className="Profile_box">
+                        <span className="Profilefile">
 
+                            <label> Name</label>
+                            <input 
+                                type="text" 
+                                onChange={((e) => {
+                                    setname(e.target.value)
+                                })}
+                                defaultValue={name}
+                            />
+                       
+                            <label>Gender</label>
+                            <select 
+                                onChange={((e) => {
+                                    setgender(e.target.value)
+                                })}
+                            >
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
 
+                            <button
+                                onClick={RandomName}
+                            >
+                                Random Name
+                            </button>
+                           
+                        </span>
+                    </div>
                 </div>
+
+
 
                 <div className="right">
-                     <p> Result </p>
+                    <p> Result </p>
+                    <img src={image} alt="" className="uploadImage"/>
 
+                    <div className="box">
 
-                     <img src={image} alt="" className="uploadImage"/>
+                        my name is {name} and the gender is {gender}
 
-
-
-
-
+                    </div>
                 </div>
-
             </div>
-
-
         </div>
     )
 }
